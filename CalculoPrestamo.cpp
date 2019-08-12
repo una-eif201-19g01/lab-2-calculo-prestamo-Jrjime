b13f2b38139888adc16d22f62018ba6c3b7ba4fe
Jrjime <Jrjime@users.noreply.github.com>
2019-08-12 02:17:49 -0600

    Se hicieron los cambios iniciales al código fuente

--- CalculoPrestamo.cpp
+++ CalculoPrestamo.cpp
@@ -4,3 +4,59 @@
 
 #include <iostream>
 #include "CalculoPrestamo.h"
+
+float CalculoPrestamo::obtenerPorcentaje(std::string porcentajeTXT)
+{
+    
+    
+    
+    
+    
+}
+
+int CalculoPrestamo::calcularTiempoEnMeses(std::string tiempoTXT)
+{
+    
+    
+    
+    
+    
+}
+
+float CalculoPrestamo::calcularInteresMensual(float balance, float tasaAnual)
+{
+    
+    
+    
+    
+}
+
+CalculoPrestamo::CalculoPrestamo()
+{
+    
+}
+
+CalculoPrestamo::CalculoPrestamo(int64_t monto)
+{
+    
+    
+}
+
+std::string CalculoPrestamo::reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT)
+{
+    
+    
+    
+}
+
+float CalculoPrestamo::getMonto()
+{
+    
+    
+}
+
+void CalculoPrestamo::setMonto(float monto)
+{
+    
+    
+}
--- CalculoPrestamo.h
+++ CalculoPrestamo.h
@@ -9,5 +9,24 @@
 #include <cstdint>
 #include <string>
 
+class CalculoPrestamo{
+    float monto;
+    float obtenerPorcentaje(std::string porcentajeTXT);
+    int calcularTiempoEnMeses(std::string tiempoTXT);
+    float calcularInteresMensual(float balance, float tasaAnual);
+
+public:
+    CalculoPrestamo();
+    CalculoPrestamo(int64_t monto);
+    std::string reporteCalculoPrestamo(std::string tiempoTXT, std::string porcentajeTXT);
+    float getMonto();
+    void setMonto(float monto);
+    
+    
+    
+    
+    
+    
+};
 
 #endif //CALCULOPRESTAMO_CALCULOPRESTAMO_H
